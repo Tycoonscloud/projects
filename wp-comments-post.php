8a7239bd75f2e103d5743b7530d3184fc081bf67
Tycoonscloud <34260764+Tycoonscloud@users.noreply.github.com>
2017-12-14 22:41:38 +0530

    Update wp-comments-post.php

--- wp-comments-post.php
+++ wp-comments-post.php
@@ -1,9 +1,4 @@
 <?php
-/**
- * Handles Comment Post to WordPress and prevents duplicate comment posting.
- *
- * @package WordPress
- */
 
 if ( 'POST' != $_SERVER['REQUEST_METHOD'] ) {
 	$protocol = $_SERVER['SERVER_PROTOCOL'];
@@ -34,26 +29,10 @@ if ( is_wp_error( $comment ) ) {
 
 $user = wp_get_current_user();
 
-/**
- * Perform other actions when comment cookies are set.
- *
- * @since 3.4.0
- *
- * @param WP_Comment $comment Comment object.
- * @param WP_User    $user    User object. The user may not exist.
- */
 do_action( 'set_comment_cookies', $comment, $user );
 
 $location = empty( $_POST['redirect_to'] ) ? get_comment_link( $comment ) : $_POST['redirect_to'] . '#comment-' . $comment->comment_ID;
 
-/**
- * Filters the location URI to send the commenter after posting.
- *
- * @since 2.0.5
- *
- * @param string     $location The 'redirect_to' URI sent via $_POST.
- * @param WP_Comment $comment  Comment object.
- */
 $location = apply_filters( 'comment_post_redirect', $location, $comment );
 
 wp_safe_redirect( $location );
